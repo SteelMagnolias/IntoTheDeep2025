2d670ee659dc604b334c8d3b60e56bfc5cefd14d
steel <steelmagnolias17009@gmail.com>
2024-11-29 17:28:05 -0500

    net zone done :)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/earlySeasonNetZone.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/earlySeasonNetZone.java
@@ -28,18 +28,20 @@ public class earlySeasonNetZone extends LinearOpMode {
     private ColorSensor colorRight;
 
     // variables
-    double pow = 0.2;
-    double armPow = 0.7;
-    int targetRed = 2000;
+    double pow = 0.4;
+    double armPow = 0.6;
     int targetBlue = 2000;
+    int targetRed = 2000;
     int targetGreen = 2000;
-    double buffer = 0.2;
-    double width = 5.125;
+    int sampleArm = -3164;
+    int parkArm =  -4000;
+    double bufferW = 0.6;
+    double bufferA = 30;
 
     @Override
     public void runOpMode() throws InterruptedException {
         //map motors from configuration to motor names in code
-        leftBack  = hardwareMap.get(DcMotor.class, "leftBack");
+        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
         rightBack = hardwareMap.get(DcMotor.class, "rightBack");
         leftFront = hardwareMap.get(DcMotor.class, "leftFront");
         rightFront = hardwareMap.get(DcMotor.class, "rightFront");
@@ -52,8 +54,10 @@ public class earlySeasonNetZone extends LinearOpMode {
         rightFront.setDirection(DcMotor.Direction.REVERSE);
         armRight.setDirection(DcMotor.Direction.REVERSE);
 
+        intake.setPower(0.5); //hold specimen in
+
         //encoder setup
-        armEncoder = armRight;
+        armEncoder = armLeft;
         armEncoder.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         // sensors
@@ -81,29 +85,42 @@ public class earlySeasonNetZone extends LinearOpMode {
 
         waitForStart();
 
-        // turn counter-clockwise, line up with net line, looking for red/blue
-        while(colorRight.red() < targetRed && colorRight.blue() < targetBlue){
+        targetBlue = colorRight.blue() + 250;
+        targetRed = colorRight.red() + 250;
+        targetGreen = colorRight.green() + 250;
+
+        colorDrive(1); // left to net zone line
+
+        //turn right to line
+        pow = 0.2;
+        while(colorRight.blue() < targetBlue && colorRight.red() < targetRed){
+            leftFront.setPower(0);
+            leftBack.setPower(0);
             rightFront.setPower(pow);
             rightBack.setPower(pow);
-            leftBack.setPower(pow/2);
         }
-        rightFront.setPower(0);
-        rightBack.setPower(0);
-        leftBack.setPower(0);
+        pow = 0.4;
 
-        //may have to strafe
+        strafeRight(100); // line up with bucket
 
-        //sample in basket
-        arm(420);//change this
-        intake(1,500);
-        arm(0);
+        driveBackwards(150); // back up to get in bucket
 
-        strafeRight(3.4); // Strafes right to hit wall and turn
-        strafeLeft(1.8); // Strafes left about 1 square to pass field samples
-        turnCC(1.54); // Turns to face the general direction of the submersible
+        arm(sampleArm); //drop sample in bucket
 
-        // line up with submersible, strafe left, looking for white
-        while(colorLeft.red() < targetRed || colorLeft.blue() < targetBlue || colorLeft.green() < targetGreen){
+        sleep(500); //wait for arm  to stop
+
+        intake(-1, 3000); // let go of sample
+
+        arm(-2500); // bring arm back into robot
+
+        turnCC(1400); //turn to face submersible
+
+        driveForward(600); // line up with space
+
+        strafeRight(750); // straten on wall
+
+        //drive left till we see white submersible line
+        while (colorLeft.red() < targetRed || colorLeft.blue() < targetBlue || colorLeft.green() < targetGreen){
             leftFront.setPower(-pow);
             leftBack.setPower(pow);
             rightFront.setPower(pow);
@@ -114,9 +131,7 @@ public class earlySeasonNetZone extends LinearOpMode {
         rightFront.setPower(0);
         rightBack.setPower(0);
 
-        distanceDrive(6); // Moving forwards, stops before submersible
-
-        arm(69); // Put arm up to park
+        arm(parkArm);
     }
 
 
@@ -204,59 +219,90 @@ public class earlySeasonNetZone extends LinearOpMode {
         rightBack.setPower(0);
     }
 
-    private void distanceDrive (double dis){
+    private void distanceDrive(double dis) {
         double DRValue = distanceRight.getDistance(DistanceUnit.INCH);
         double DLValue = distanceLeft.getDistance(DistanceUnit.INCH);
         double DAValue = (DRValue+DLValue)/2 - dis;
 
-        while(DAValue > buffer || DAValue < -buffer){
+        while(DAValue > bufferW || DAValue < - bufferW){
             //variables
             DRValue = distanceRight.getDistance(DistanceUnit.INCH);
             DLValue = distanceLeft.getDistance(DistanceUnit.INCH);
             DAValue = (DRValue+DLValue)/2 - dis;
 
-            // forwards backwards movement
-            pow = 2*((DAValue + dis) / 30) - dis;
+            if (DRValue > DLValue + 5){
+                leftFront.setPower(pow);
+                leftBack.setPower(pow);
+                rightFront.setPower(-pow);
+                rightBack.setPower(-pow);
+            } else  if (DRValue > DLValue + 5) {
+                leftFront.setPower(-pow);
+                leftBack.setPower(-pow);
+                rightFront.setPower(pow);
+                rightBack.setPower(pow);
+            } else {
+                double lp = (DLValue + 0.1 - dis) * 0.08;
+                double rp = (DRValue + 0.1 - dis) * 0.08;
 
-            //turning movement
-            double dir = 1;
-            if(DLValue > DRValue) dir = -1;
-            double theta = dir * (90 - Math.abs (Math.atan (width / (DLValue - DRValue))));
-            if (theta > 0) theta += 10;
-            if (theta < 0) theta -= 10;
-            double thetaPow = 2 * ((theta+100)/200) - 1;
+                leftFront.setPower(lp);
+                leftBack.setPower(lp);
+                rightFront.setPower(rp);
+                rightBack.setPower(rp);
+            }
 
-            //set wheel power
-            leftFront.setPower(pow - thetaPow);
-            leftBack.setPower(pow - thetaPow);
-            rightFront.setPower(pow + thetaPow);
-            rightBack.setPower(pow + thetaPow);
+            telemetry.addData("distanceLeft:", distanceLeft.getDistance(DistanceUnit.INCH));
+            telemetry.addData("distanceRight:", distanceRight.getDistance(DistanceUnit.INCH));
+            telemetry.update();
         }
-
-        //reset wheels and pow
         leftFront.setPower(0);
         leftBack.setPower(0);
         rightFront.setPower(0);
         rightBack.setPower(0);
-
-        pow = 4;
     }
 
-    private void arm(int dis){
-        while (armEncoder.getCurrentPosition() < dis - 25) { //move arm up
-            armRight.setPower(armPow);
+    private void arm(int dis) {
+        while (armEncoder.getCurrentPosition() < dis - bufferA || armEncoder.getCurrentPosition() > dis + bufferA) {
+            double armPos = armEncoder.getCurrentPosition();
+
+            armPow = (armPos + 50 - dis) * -0.0025;
+
             armLeft.setPower(armPow);
-        }
-        while (armEncoder.getCurrentPosition() > dis + 25) { // move arm down
-            armRight.setPower(-armPow);
-            armLeft.setPower(-armPow);
+            armRight.setPower(armPow);
+
+            telemetry.addData("arm encoder", armEncoder.getCurrentPosition());
+            telemetry.update();
         }
         armLeft.setPower(0);
-        armLeft.setPower(0);
+        armRight.setPower(0);
+    }
+
+    public void colorDrive (int dir){
+        pow = 0.2;
+        while (colorLeft.red() < targetRed && colorLeft.blue() < targetBlue){ //drive back till we see blue line then stop
+            leftFront.setPower(pow*dir);
+            leftBack.setPower(pow*dir);
+            rightBack.setPower(pow*dir);
+            rightFront.setPower(pow*dir);
+
+            telemetry.addData("colorRight Red:", colorRight.red());
+            telemetry.addData("colorRight Blue:", colorRight.blue());
+
+            telemetry.addData("target red", targetRed);
+            telemetry.addData("target blue", targetBlue);
+
+            telemetry.update();
+        }
+
+        leftFront.setPower(0);
+        leftBack.setPower(0);
+        rightBack.setPower(0);
+        rightFront.setPower(0);
+
+        pow = 0.4;
     }
 
     private void intake(int dir, double t){
-        intake.setPower(pow*dir);
+        intake.setPower(1*dir);
         sleep((long) t);
         intake.setPower(0);
     }
