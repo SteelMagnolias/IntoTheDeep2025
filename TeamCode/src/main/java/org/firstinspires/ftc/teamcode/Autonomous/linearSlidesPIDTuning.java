b1d6bc80d40aa829335357ad9956003e4c54df6a
steel <steelmagnolias17009@gmail.com>
2024-12-28 19:03:06 -0500

    added PID tuning for linear slides

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/linearSlidesPIDTuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/linearSlidesPIDTuning.java
@@ -5,18 +5,18 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@Autonomous(name = "armPIDTuning", group = "Iterative Opmode")
-public class armPIDTuning extends OpMode {
+@Autonomous(name = "linearSlidesPIDTuning", group = "Iterative Opmode")
+public class linearSlidesPIDTuning extends OpMode {
 
-    private DcMotor armLeft;
-    private DcMotor armRight;
+    private DcMotor linearSlidesLeft;
+    private DcMotor linearSlidesRight;
     private DcMotor armEncoder;
 
     ElapsedTime armTimer = new ElapsedTime();
 
-    double armPow;
-    double desArmPos;
-    double armPos;
+    double linearSlidesPow;
+    double desLength;
+    double ArmLength;
     double currentTime;
     double previousTime;
     double currentError;
@@ -29,13 +29,11 @@ public class armPIDTuning extends OpMode {
     double KD = 0.5;
 
     public void init() {
-        armLeft = hardwareMap.get(DcMotor.class, "armLeft");
-        armRight = hardwareMap.get(DcMotor.class, "armRight");
-
-        armRight.setDirection(DcMotor.Direction.REVERSE);
+        linearSlidesLeft = hardwareMap.get(DcMotor.class, "linearSlidesLeft");
+        linearSlidesRight = hardwareMap.get(DcMotor.class, "linearSlidesRight");
 
         //encoder setup
-        armEncoder = armLeft;
+        armEncoder = linearSlidesLeft;
 
         armEncoder.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
@@ -45,29 +43,29 @@ public class armPIDTuning extends OpMode {
         double lefty2 = -(gamepad2.left_stick_y); // this is the value of gamepad2's left joystick y value
         boolean a2 = gamepad2.a; // this is the value of the a button on gamepad2
 
-        desArmPos += lefty2 * 3;
+        desLength += lefty2 * 3;
 
         if(a2) {
             //PID stuff
-            armPos = -armEncoder.getCurrentPosition();
-            currentError = armPos - desArmPos;
+            ArmLength = -armEncoder.getCurrentPosition();
+            currentError = ArmLength - desLength;
             currentTime = armTimer.milliseconds();
 
             P = currentError * KP;
             I = KI * (currentError * (currentTime - previousTime));
             D = KD * (currentError - previousError) / (currentTime - previousTime);
-            armPow = (P + I + D);
+            linearSlidesPow = (P + I + D);
 
             previousTime = currentTime;
             previousError = currentError;
 
-            armLeft.setPower(armPow);
-            armRight.setPower(armPow);
+            linearSlidesLeft.setPower(linearSlidesPow);
+            linearSlidesRight.setPower(linearSlidesPow);
         }
 
-        telemetry.addData("armPow", armPow);
-        telemetry.addData("desired arm position", desArmPos);
-        telemetry.addData("arm position", armPos);
+        telemetry.addData("linearSlidesPow", linearSlidesPow);
+        telemetry.addData("desired length", desLength);
+        telemetry.addData("length", ArmLength);
         telemetry.update();
     }
 }
